6783dc47be1d19633dec78b7973965770576b426
María Lourdes <35180135+Maria-Lour@users.noreply.github.com>
2023-06-15 20:49:49 -0300

    React. Tarea clase 2
    Se descargó el template de calculadora desde el repositorio del profesor. Se agregó una lista Select con menú desplegable para que el usuario elija qué operador usar. Luego al apretar el botón calcular se hacen los cálculos y los muestra en el div #resultado.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-
 function calcular() {
-    // recuperar los valores de la página
-    // sumarlos
-    // agregarlos en la página en el lugar adecuado
+    numero1 = Number(document.querySelector("#op1").value);
+    numero2 = Number(document.querySelector("#op2").value);
+    operador = document.querySelector("#operadores");
+    var operadorelegido = operador.options[operador.selectedIndex].value;
+
+    if (operadorelegido === "sumar") {
+        resultado = numero1 + numero2;
+    } else if (operadorelegido === "restar") {
+        resultado = numero1 - numero2;
+    } else if (operadorelegido === "dividir") {
+            resultado = numero1 / numero2;
+    } else if (operadorelegido === "multiplicar") {
+        resultado = numero1 * numero2;
+    }  
+    document.querySelector("#resultado").innerText = resultado;
 }
